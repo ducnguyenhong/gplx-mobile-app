6b49df4e10352f6629c1c7e0505e69d1075595af
tuananh279 <tuananhptk4@gmail.com>
2022-09-17 04:03:29 +0700

    code review question

--- src/screens/review-question/review-question.style.js
+++ src/screens/review-question/review-question.style.js
@@ -4,5 +4,32 @@ export const styles = StyleSheet.create({
   savMain: {
     backgroundColor: '#FFF',
     flex: 1
+  },
+  reviewItem: {
+    flexDirection: 'column',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
+  itemHeader: {
+    fontSize: 18,
+    lineHeight: 32,
+    fontWeight: '700'
+  },  
+  itemDescription: {
+    fontSize: 16,
+    lineHeight: 24
+  },
+  itemProgress: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'space-between'
+  },
+  itemCount: {
+    color: '#2ecc71',
+    fontSize: 16,
+    fontWeight: '600',
+    textAlign: 'right'
   }
 });
